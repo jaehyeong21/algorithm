c0633f61318cfffcd7b50df76f45534d3dfef955
Lee jae hyeong <ftko6252@naver.com>
2024-03-18 20:45:17 +0900

    boj2061

--- src/Boj10825.java
+++ src/Boj10825.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-class Student implements Comparable<Student>{
+class Student implements Comparable<Student> {
     String name;
     int kor;
     int eng;
     int math;
 
-    public Student(String name, int kor, int eng, int math){
+    public Student(String name, int kor, int eng, int math) {
         this.name = name;
         this.kor = kor;
         this.eng = eng;
@@ -16,43 +16,42 @@ class Student implements Comparable<Student>{
     }
 
     @Override
-    public int compareTo(Student o)
-    {
-        if(this.kor == o.kor){
-            if(this.eng == o.eng){
-                if(this.math == o.math){
-                    return this.name.compareTo(o.name);
-                }else{
-                    return o.math - this.math;
+    public int compareTo(Student o) {
+        if (this.kor == o.kor) { // 1. 국어 점수가 같을 경우
+            if (this.eng == o.eng) { // 2. 영어 점수가 같을 경우
+                if (this.math == o.math) { // 3. 수학 점수가 같을 경우
+                    return this.name.compareTo(o.name); // 4. 이름을 사전순으로 비교하여 반환
+                } else { // 3. 수학 점수가 다를 경우
+                    return Integer.compare(o.math, this.math); // 수학 점수가 감소하는 순서로 비교하여 반환
                 }
-            }else{
-                return this.eng - o.eng;
+            } else { // 2. 영어 점수가 다를 경우
+                return Integer.compare(this.eng, o.eng); // 영어 점수가 증가하는 순서로 비교하여 반환
             }
-        }else{
-            return o.kor - this.kor;
+        } else { // 1. 국어 점수가 다를 경우
+            return Integer.compare(o.kor, this.kor); // 국어 점수가 감소하는 순서로 비교하여 반환
         }
     }
 }
 
 public class Boj10825 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        sc.nextLine();
+        sc.nextLine(); // 개행문자 처리
 
         ArrayList<Student> students = new ArrayList<>();
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             String[] strings = sc.nextLine().split(" ");
             String name = strings[0];
             int kor = Integer.parseInt(strings[1]);
             int eng = Integer.parseInt(strings[2]);
             int math = Integer.parseInt(strings[3]);
-            students.add(new Student(name,kor,eng,math));
+            students.add(new Student(name, kor, eng, math));
         }
         Collections.sort(students);
 
-        for(int i = 0; i < students.size(); i++){
+        for (int i = 0; i < students.size(); i++) {
             System.out.println(students.get(i).name);
         }
     }
